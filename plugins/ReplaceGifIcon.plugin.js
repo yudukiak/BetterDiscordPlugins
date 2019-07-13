57ba8d0bf202d3c622910bfef2b6af9d5e9c3847
micelle <micelle_yamame@hotmail.co.jp>
2019-07-13 12:00:06 +0900

    [update] 通話中のアイコンに対する処理を変更
    ついでに簡単な言語対応

--- plugins/ReplaceGifIcon.plugin.js
+++ plugins/ReplaceGifIcon.plugin.js
@@ -1,25 +1,33 @@
 //META{"name":"ReplaceGifIcon","displayName":"micelle","website":"https://github.com/micelle","source":"https://github.com/micelle/dc_BetterDiscordPlugins"}*//
 
+const lang = document.documentElement.lang;
+
 const ReplaceGifIcon = function() {};
 ReplaceGifIcon.prototype.errorList = [];
-ReplaceGifIcon.prototype.log = (msg, data) => {
-  const name = ReplaceGifIcon.prototype.getName();
+ReplaceGifIcon.prototype.log = function(msg, data) {
+  const name = this.getName();
   console.log(`%c[${name}] ${msg}`, 'color:#C9242F', data);
 };
-ReplaceGifIcon.prototype.start = () => ReplaceGifIcon.prototype.log('start', ReplaceGifIcon.prototype.getVersion());
-ReplaceGifIcon.prototype.load = () => ReplaceGifIcon.prototype.log('load', ReplaceGifIcon.prototype.getVersion());
-ReplaceGifIcon.prototype.unload = () => ReplaceGifIcon.prototype.log('unload', ReplaceGifIcon.prototype.getVersion());
-ReplaceGifIcon.prototype.stop = () => ReplaceGifIcon.prototype.log('stop', ReplaceGifIcon.prototype.getVersion());
-ReplaceGifIcon.prototype.onMessage = () => {};
-ReplaceGifIcon.prototype.onSwitch = () => {};
-ReplaceGifIcon.prototype.observer = (e) => {
-  // .da-image: アイコン
-  // .da-avatarSmall: 通話中のリスト
-  // .stop-animation: 自分のアイコン
+ReplaceGifIcon.prototype.start = function() {
+  this.log('start', this.getVersion());
+};
+ReplaceGifIcon.prototype.load = function() {
+  this.log('load', this.getVersion());
+};
+ReplaceGifIcon.prototype.unload = function() {
+  this.log('unload', this.getVersion());
+};
+ReplaceGifIcon.prototype.stop = function() {
+  this.log('stop', this.getVersion());
+};
+ReplaceGifIcon.prototype.onMessage = function() {};
+ReplaceGifIcon.prototype.onSwitch = function() {};
+ReplaceGifIcon.prototype.observer = function(e) {
+  // .da-image: アイコン, .da-avatarSmall: 通話中のリスト, .stop-animation: 自分のアイコン
   $('.da-image, .da-avatarSmall, .stop-animation').each((index, element) => {
     // チェック済みは処理しない
-    const name = ReplaceGifIcon.prototype.getName();
-    if($(element).data(name)) return;
+    const name = this.getName();
+    if ($(element).data(name)) return;
     $(element).data(name, true);
     // styleを持ってなければ処理しない
     const style = $(element).css('background-image');
@@ -28,19 +36,21 @@ ReplaceGifIcon.prototype.observer = (e) => {
     const URL = style.match(/url\("(.+)"\)/)[1]; // 画像のURLを抜き出す
     const gifURL = URL.replace(/\.(png|webp)/, '.gif'); // 画像のURLをGIF用に置換
     const fileName = URL.match(/\/([^/]*)\.(png|webp)/)[1]; // ファイル名を抜き出す
-    // デフォルトアイコンは処理しない
-    if (!/cdn\.discordapp\.com/.test(gifURL)) return;
-    // errorListに居たら処理しない
-    if (ReplaceGifIcon.prototype.errorList[fileName]) return;
+    if (!/cdn\.discordapp\.com/.test(gifURL)) return; // デフォルトアイコンは処理しない
+    if (this.errorList[fileName]) return; // errorListに居たら処理しない
     // 画像の読み込み
     const image = new Image();
     image.src = gifURL;
     image.style = 'width:100%';
-    image.onload = () => $(element).css('overflow', 'hidden').html(image);
-    image.onerror = () => ReplaceGifIcon.prototype.errorList[fileName] = 1;
+    image.onload = () => {
+      ($(element).hasClass('da-avatarSmall'))
+      ? $(element).css('background-image', `url(${gifURL})`)
+      : $(element).css('overflow', 'hidden').html(image);
+    };
+    image.onerror = () => this.errorList[fileName] = 1;
   });
 };
 ReplaceGifIcon.prototype.getName = () => 'ReplaceGifIcon';
-ReplaceGifIcon.prototype.getDescription = () => 'GIFアニメーションのアイコンに置き換えます。\nReplace with an GIF animated icon.';
-ReplaceGifIcon.prototype.getVersion = () => '1.0.2';
+ReplaceGifIcon.prototype.getDescription = () => lang === 'ja' ? 'GIFアニメーションのアイコンに置き換えます。' : 'Replace with an GIF animated icon.';
+ReplaceGifIcon.prototype.getVersion = () => '1.0.3';
 ReplaceGifIcon.prototype.getAuthor = () => 'micelle';
